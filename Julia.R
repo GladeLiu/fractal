b99d9d7dbc871da5c8482b4f3b5ecef2617b3a72
Chenang Liu <liuchenang@gmail.com>
2013-10-31 11:22:52 +0800

    renew function
    renew the two function which was pushed yesterday

--- Julia.R
+++ Julia.R
@@ -1,24 +1,31 @@
-Julia<-function(c,res,iter,xc,yc,xoom){
-#library(pracma);
-x0<-xc-2/xoom;
-x1<-xc+2/xoom;
-y0<-yc-2/xoom;y1=yc+2/xoom;
-x<-seq(x0,x1,length=res);
-y<-seq(y0,y1,length=res);
-xx<-t(matrix(rep(x,length(x)),length(x)));
-yy<-matrix(rep(y,length(y)),length(y));
-z<-xx+yy*(1i);
-N<-zeros(res,res);
-C<-c*ones(res,res);
-for(k in 1:iter){
-   z=z^2+C;
-   N[abs(z)>2]=k;
-   C[abs(z)>2]=0;
-   z[abs(z)>2]=0;
-}
-image(x,y,N,col=rainbow(max(N)));
+Julia<-function(c=1i,res=512,iter=200,xc=0,yc=0,
+  xoom=1,color=terrain.colors(30),...){
+ #Julia set
+ #c is the argument;res is the target resolution;
+ #iter is cycle index; (xc,yc) is the center of the graphic;
+ #xoom is the magnification times;
+  x0<-xc-2/xoom;x1<-xc+2/xoom;
+  y0<-yc-2/xoom;y1=yc+2/xoom;
+  x<-seq(x0,x1,length=res);
+  y<-seq(y0,y1,length=res);
+  xx<-t(matrix(rep(x,length(x)),length(x)));
+  yy<-matrix(rep(y,length(y)),length(y));
+  z<-xx+yy*(1i);
+  N<-matrix(0,res,res);
+  C<-c*matrix(1,res,res);
+   for(k in 1:iter){
+     z=z^2+C;
+     N[abs(z)>2]=k;
+     C[abs(z)>2]=0;
+     z[abs(z)>2]=0;
+   }
+  image(x,y,N,col=color,...);
 }
 
-Julia(1i,512,200,0,0,1);
-Julia(1i,512,200,0,0,2000);
-Julia(-0.8-0.21i,512,200,0,0,1);
+#example
+par(mar=rep(0,4));
+Julia(1i,512,200,0,0,1,xlab="",ylab="",axes = FALSE);
+dev.new();par(mar=rep(0,4));
+Julia(1i,512,200,0,0,2000,xlab="",ylab="",axes = FALSE);
+dev.new();par(mar=rep(0,4));
+Julia(-1.8-0.21i,512,200,0,0,1,xlab="",ylab="",axes = FALSE);
--- Mandelbrot.R
+++ Mandelbrot.R
@@ -1,22 +1,28 @@
-Mandelbrot<-function(res,iter,xc,yc,xoom){
-x0=xc-2/xoom;x1=xc+2/xoom;
-y0=yc-2/xoom;y1=yc+2/xoom;
-x=seq(x0,x1,length=res);
-y=seq(y0,y1,length=res);
-xx<-t(matrix(rep(x,length(x)),length(x)));
-yy<-matrix(rep(y,length(y)),length(y));
-z<-xx+yy*(1i);            
-C=z;
-N=zeros(res,res); 
- 
-for(k in 1:iter){     
-   z=z^2+C;        
-   N[abs(z)>4]=k;  
-   z[abs(z)>4]=0;
-   C[abs(z)>4]=0;
-}
-image(x,y,N,col=rainbow(max(N))); 
+Mandelbrot<-function(res=512,iter=128,xc=0,yc=0,
+  xoom=1,color=terrain.colors(30),...){
+ #Mandelbrot set
+ #res is the target resolution;
+ #iter is cycle index; (xc,yc) is the center of the graphic;
+ #xoom is the magnification times;
+  x0<-xc-2/xoom;x1<-xc+2/xoom;
+  y0<-yc-2/xoom;y1<-yc+2/xoom;
+  x<-seq(x0,x1,length=res);
+  y<-seq(y0,y1,length=res);
+  xx<-t(matrix(rep(x,length(x)),length(x)));
+  yy<-matrix(rep(y,length(y)),length(y));
+  z<-xx+yy*(1i);C<-z;
+  N<-matrix(0,res,res); 
+  for(k in 1:iter){     
+    z=z^2+C;        
+    N[abs(z)>4]=k;  
+    z[abs(z)>4]=0;
+    C[abs(z)>4]=0;
+  }
+ image(x,y,N,col=color,...); 
 }
 
-Mandelbrot(512,100,0,0,1)
-Mandelbrot(512,128,-1.478,0,300)
+#example
+par(mar=rep(0,4));
+Mandelbrot(512,100,0,0,1,xlab="",ylab="",axes = FALSE);
+dev.new();par(mar=rep(0,4));
+Mandelbrot(512,128,-1.478,0,300,xlab="",ylab="",axes = FALSE);
